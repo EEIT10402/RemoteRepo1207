5f509f7910ca8f89aa4ec8e966051f7f68dc34cb
朱薇 <@163.com>
2018-12-07 14:32:12 +0800

    第2次
    加了幾個空格應付

--- LabWebHibernateSpring/src/main/java/misc/EncodingFilter.java
+++ LabWebHibernateSpring/src/main/java/misc/EncodingFilter.java
@@ -18,7 +18,7 @@ public class EncodingFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		this.filterConfig = filterConfig;
-	}
+	}  
 	@Override
 	public void doFilter(ServletRequest req,
 			ServletResponse resp, FilterChain chain) throws IOException, ServletException {
